873ec37ad228d8e0a71377544b9ec88aa2dc249a
m-schmutz <marschm131@gmail.com>
2023-05-30 17:50:30 -0700

    added find chain function

--- rust_sim/src/main.rs
+++ rust_sim/src/main.rs
@@ -1,6 +1,6 @@
-use rand::seq::SliceRandom;
 use std::collections::HashMap;
-use std::hash::Hash;
+use std::collections::HashSet;
+use rand::seq::SliceRandom;
 use std::io;
 // use num_cpus;
 
@@ -11,6 +11,7 @@ fn _get_num_trials() -> usize {
     
     let trials: usize;
 
+    //  loop until valid input
     loop {
         println!("Enter the number of trials that you want to simulate: ");
         
@@ -45,7 +46,6 @@ fn shuffle_boxes() -> HashMap<usize, usize> {
     let mut randomized: Vec<usize> = (0..100).collect();
     randomized.shuffle(&mut rand::thread_rng());
 
-
     // convert the vector to a hashmap
     let boxes = randomized.iter().enumerate().map(|(i, num)| (i, *num)).collect::<HashMap<usize, usize>>();
 
@@ -54,8 +54,47 @@ fn shuffle_boxes() -> HashMap<usize, usize> {
 }
 
 
+fn find_chain(boxes: &mut HashMap<usize, usize>, box_num: usize) -> bool {
+    // get the first number in the chain from the first box
+    let mut num: usize = *boxes.get(&box_num).unwrap();
+
+    // length is always at minimum 1
+    let mut length: usize = 1;
+
+    // keep track of visited boxes
+    let mut visited_boxes: HashSet<usize> = vec![box_num].into_iter().collect();
+
+    // while the chain length is 50 or less 
+    // and the chain is not complete
+    while (length < 51) && (num != box_num) {
+        // add new box to visited boxes
+        visited_boxes.insert(num);
+
+        // update num and length
+        length += 1;
+        num = *boxes.get(&num).unwrap();
+    }
+
+    // remove boxes that have been visited
+    boxes.retain(|&key, _ | !visited_boxes.contains(&key));
+
+    // return the success of the trial
+    return length < 51;
+}
+
+
+
+
+
+
+
 fn run_trial(boxes: HashMap<usize, usize>) -> bool {
-    // 
+
+    let mut boxes = 0;
+
+
+    // to check whether a given shuffle is winning or not
+    // we need to check if a chain longer than 50 exists
     for (box_num, number) in boxes.iter(){
         println!("box_num: {} | number: {}", box_num,  number);
     }
@@ -70,7 +109,7 @@ fn main() {
     // let trials: usize = get_num_trials();
     // println!("Running {} trials", trials);
 
-    let boxes = shuffle_boxes();
+    let boxes: HashMap<usize, usize> = shuffle_boxes();
 
     run_trial(boxes);
     
